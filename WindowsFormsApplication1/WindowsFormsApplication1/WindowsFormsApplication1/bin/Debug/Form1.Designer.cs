4286a6b09013083e548e6444ae8aa852896df553
codeziwei <codeziwei@gmail.com>
2013-05-16 17:55:50 +0800

    78

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/WindowsFormsApplication1/bin/Debug/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLookfile = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnLookfile2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnFormat = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnLookfile
+            // 
+            this.btnLookfile.Location = new System.Drawing.Point(551, 19);
+            this.btnLookfile.Name = "btnLookfile";
+            this.btnLookfile.Size = new System.Drawing.Size(80, 37);
+            this.btnLookfile.TabIndex = 1;
+            this.btnLookfile.Text = "浏览..";
+            this.btnLookfile.UseVisualStyleBackColor = true;
+            this.btnLookfile.Click += new System.EventHandler(this.btnLookfile_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("宋体", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(11, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(539, 24);
+            this.textBox1.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnLookfile2
+            // 
+            this.btnLookfile2.Location = new System.Drawing.Point(556, 28);
+            this.btnLookfile2.Name = "btnLookfile2";
+            this.btnLookfile2.Size = new System.Drawing.Size(75, 35);
+            this.btnLookfile2.TabIndex = 1;
+            this.btnLookfile2.Text = "浏览..";
+            this.btnLookfile2.UseVisualStyleBackColor = true;
+            this.btnLookfile2.Click += new System.EventHandler(this.btnLookfile2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(11, 37);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(546, 21);
+            this.textBox2.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.btnLookfile);
+            this.groupBox1.Location = new System.Drawing.Point(21, 20);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(679, 68);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "模版文件路径";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.btnLookfile2);
+            this.groupBox2.Location = new System.Drawing.Point(21, 112);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(679, 115);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "需要格式化的Excel文件所在文件夹";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "格式化选项";
+            // 
+            // btnFormat
+            // 
+            this.btnFormat.Location = new System.Drawing.Point(21, 248);
+            this.btnFormat.Name = "btnFormat";
+            this.btnFormat.Size = new System.Drawing.Size(679, 23);
+            this.btnFormat.TabIndex = 5;
+            this.btnFormat.Text = "开始格式化";
+            this.btnFormat.UseVisualStyleBackColor = true;
+            this.btnFormat.Click += new System.EventHandler(this.btnFormat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 462);
+            this.Controls.Add(this.btnFormat);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MaximumSize = new System.Drawing.Size(750, 1600);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "中教育人-Excel格式化";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLookfile;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnLookfile2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnFormat;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/WindowsFormsApplication1/bin/Debug/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    /// <summary>
+    /// Excle 文件按模板格式转换
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region 浏览选文件
+        private void btnLookfile_Click(object sender, EventArgs e)
+        {
+            //初始化一个OpenFileDialog类
+            OpenFileDialog fileDialog = new OpenFileDialog();
+
+            fileDialog.DefaultExt = "xls";
+            fileDialog.Filter =
+                "Excle files (*.xls)|*.xls|Excle files (*.xlsx)|*.xlsx";
+            //fileDialog.DefaultExt = ".xls,xlsx";
+            fileDialog.Title = "请选择模版文件";
+
+
+
+            //判断用户是否正确的选择了文件
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //获取用户选择文件的后缀名
+                string extension = Path.GetExtension(fileDialog.FileName);
+                //声明允许的后缀名
+                string[] str = new string[] { ".xls", ".xlsx" };
+                if (!str.Contains(extension))
+                {
+                    MessageBox.Show("请选择excle格式文件！","提示",MessageBoxButtons.OK);
+                }
+                else
+                {
+                    //获取用户选择的文件，并判断文件大小不能超过20K，fileInfo.Length是以字节为单位的
+                    FileInfo fileInfo = new FileInfo(fileDialog.FileName);
+                    textBox1.Text = fileDialog.FileName;
+                }
+            }
+        }
+        #endregion
+
+        #region 浏览选文件夹
+        private void btnLookfile2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            //folderBrowserDialog.f
+
+            folderBrowserDialog.Description = "选择要格式化的的Excel所存放的文件夹";
+            
+            // Do not allow the user to create new files via the FolderBrowserDialog.
+            folderBrowserDialog.ShowNewFolderButton = false;
+
+            // Default to the My Documents folder.
+            folderBrowserDialog.RootFolder = Environment.SpecialFolder.Desktop; 
+
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = folderBrowserDialog.SelectedPath;
+            }
+         
+
+        }
+        #endregion
+
+        #region 格式化 
+        private void btnFormat_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("请选择模板", "提示", MessageBoxButtons.OK);
+            }
+            //获取模板数据信息
+            DataSet ds = ToDataTable(textBox1.Text);
+            DataTable dt = ds.Tables[0];
+            List<String> listColumName = new List<string>();
+            if (dt.Columns.Count > 0)
+            {
+                foreach (DataColumn item in dt.Columns)
+                {
+                    listColumName.Add(item.ColumnName);
+                }
+            }
+            foreach (DataRow dr in dt.Rows)
+            {
+                for (int i = 0; i < dt.Columns.Count; i++)
+                {
+                   if(!String.IsNullOrEmpty(dr[i].ToString()))
+                   {
+                       listColumName[i] = listColumName[i] + "," + dr[i].ToString();
+                   }
+                }
+            }
+
+            //遍历文件夹下所有Excel文件
+            List<String> listFilePath = new List<string>();
+            GetFileListByFolder(textBox2.Text, ref listFilePath);
+            Int32 iFileCount = listFilePath.Count;
+
+
+
+            
+            
+        }
+        #endregion
+
+        #region 遍历文件夹下所有Excle文件
+        /// <summary>
+        /// 文件夹下
+        /// </summary>
+        /// <param name="folder"></param>
+        /// <param name="listFilePath"></param>
+        public static void GetFileListByFolder(String folder,  ref List<String> listFilePath)
+        {
+            DirectoryInfo TheFolder = new DirectoryInfo(folder);
+            //遍历文件
+            foreach (FileInfo NextFile in TheFolder.GetFiles())
+            {
+
+                string extension = Path.GetExtension(NextFile.FullName);
+                //声明允许的后缀名
+                string[] str = new string[] { ".xls", ".xlsx" };
+                if (str.Contains(extension))
+                {
+                    listFilePath.Add(NextFile.FullName);
+                }
+                
+                
+            }
+            //遍历文件夹下
+            foreach (DirectoryInfo NextFolder in TheFolder.GetDirectories())
+            {
+                GetFileListByFolder(NextFolder.FullName, ref listFilePath);
+            }
+            
+        }
+        #endregion
+
+        #region 读取Excel文件到DataSet中
+        /// <summary>  
+        /// 读取Excel文件到DataSet中  
+        /// </summary>  
+        /// <param name="filePath">文件路径</param>  
+        /// <returns></returns>  
+        public static DataSet ToDataTable(string filePath)
+        {
+            string connStr = "";
+            string fileType = System.IO.Path.GetExtension(filePath);
+            if (string.IsNullOrEmpty(fileType)) return null;
+
+            if (fileType == ".xls")
+                connStr = "Provider=Microsoft.Jet.OLEDB.4.0;" + "Data Source=" + filePath + ";" + ";Extended Properties=\"Excel 8.0;HDR=YES;IMEX=1\"";
+            else
+                connStr = "Provider=Microsoft.ACE.OLEDB.12.0;" + "Data Source=" + filePath + ";" + ";Extended Properties=\"Excel 12.0;HDR=YES;IMEX=1\"";
+            string sql_F = "Select * FROM [{0}]";
+
+            OleDbConnection conn = null;
+            OleDbDataAdapter da = null;
+            DataTable dtSheetName = null;
+
+            DataSet ds = new DataSet();
+            try
+            {
+                // 初始化连接，并打开  
+                conn = new OleDbConnection(connStr);
+                conn.Open();
+
+                // 获取数据源的表定义元数据                         
+                string SheetName = "";
+                dtSheetName = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[] { null, null, null, "TABLE" });
+
+                // 初始化适配器  
+                da = new OleDbDataAdapter();
+                for (int i = 0; i < dtSheetName.Rows.Count; i++)
+                {
+                    SheetName = (string)dtSheetName.Rows[i]["TABLE_NAME"];
+
+                    if (SheetName.Contains("$") && !SheetName.Replace("'", "").EndsWith("$"))
+                    {
+                        continue;
+                    }
+
+                    da.SelectCommand = new OleDbCommand(String.Format(sql_F, SheetName), conn);
+                    DataSet dsItem = new DataSet();
+                    da.Fill(dsItem, SheetName);
+
+                    ds.Tables.Add(dsItem.Tables[0].Copy());
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("打开模板文件异常", "提示", MessageBoxButtons.OK);
+            }
+            finally
+            {
+                // 关闭连接  
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                    da.Dispose();
+                    conn.Dispose();
+                }
+            }
+            return ds;
+        }
+        #endregion
+    }
+}
